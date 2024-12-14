6dbe205e325eb832fb03db8573be47a7cad0be40
zAlexisD <alexis.doan@ensea.fr>
2024-12-14 02:52:11 +0100

    InterfaceKbdDB.cpp/.h commit 1.3 Add the header and csv file generator

--- InterfaceKbdDB.cpp
+++ InterfaceKbdDB.cpp
@@ -26,7 +26,7 @@ string UserID::getPassphrase() {
 
 // Method for setters structure on all attributes except username
 template <typename T>
-void UserID::setParameter(T UserID::*attribute,const T& paramInterface,const vector<const string>& T& paramList){
+void UserID::setParameter(T UserID::*attribute,const T& paramInterface,const vector<string>& paramList){
     // Init
     string userInput;
     // User prompt input
@@ -57,9 +57,11 @@ void UserID::setUsername() {
     // Init
     string usernameInput;
     // User prompt input
-    cout << "What is your name?"<< endl;
+    cout << "\nWhat is your name?"<< endl;
     cin >> usernameInput;
     // Handle non-valid username case = empty entry
+    // WIP : not very well handled but still works
+    // WIP : handle composed name
     if (usernameInput.empty()){
         while(usernameInput.empty()) {
             cout << "You require a username to continue. Please type a username"<< endl;
@@ -97,6 +99,13 @@ void UserID::setPassphrase() {
         this->passphrase = passphraseList[2];
     }
 }
+// Gather all the previous methods in one call
+void UserID::gatherInformation() {
+    setUsername();
+    setGender();
+    setDominantHand();
+    setPassphrase();
+}
 
 // Display attributes method
 void UserID::displayUserID() const {
@@ -110,29 +119,95 @@ void UserID::displayUserID() const {
 vector<vector<int>> getUserData(UserID userSettings,libevdev* dev) {
     // Inits
     int attempts = 0;
-    vector<vector<int>> keyboardData(maxPassphraseAttempt);
+    vector<vector<int>> userData(maxPassphraseAttempt);
     //
-    cout << "The passphrase to type is: " << userSettings.getPassphrase() << endl;
+    cout << "\nThe passphrase to type " << maxPassphraseAttempt << " times is: " << userSettings.getPassphrase() << endl;
     // Main loop
     while (attempts < maxPassphraseAttempt) {
         attempts++;
-        cout << "Attempt: " << attempts <<"/"<< maxPassphraseAttempt << endl;
-        cout << "\nPlease type '" << userSettings.getPassphrase() << "' and press Enter to finish" << endl;
+        cout << "\nAttempt: " << attempts <<"/"<< maxPassphraseAttempt << endl;
+        cout << "Please type '" << userSettings.getPassphrase() << "' and press Enter to finish" << endl;
         KeyCapture timeCapture = captureKeyboardEvents(dev);
         // If error detected in the passphrase typed, do it again
         if (timeCapture.getUserInput() != userSettings.getPassphrase()) {
             timeCapture.resetCapture();
             attempts--;
-            cout << "Wrong passphrase! Try again please" << endl;
+            cout << "\nWrong passphrase! Try again please :)\n";
         }
         else {
             // Convert the time Measures into 1-row of interpretable data
             const vector<int> timeData = timeToData(timeCapture.getTimeMeasure());
             // Copy the data row in keyboardData
-            keyboardData[attempts-1] = timeData;
+            userData[attempts-1] = timeData;
         }
     }
-    return keyboardData;
+    return userData;
 }
 
-// WIP : store data
+// Generate the header for the csv file according to the number of characters of the passphrase
+vector<string> genCsvHeader(UserID userID) {
+    // Init
+    const int passphraseSize = userID.getPassphrase().size();
+    vector<string> csvHeader;
+    // First add the 3 attributes of the users
+    const string userAttributes = "User,Gender,Handedness,";
+    csvHeader.push_back(userAttributes);
+    // Define for each feature the header : nb_characters-1 columns
+    for (int feature = 1; feature <= nb_features; feature++) {
+        for (int featureCol = 1; featureCol <= passphraseSize-1; featureCol++) {
+            ostringstream oss;
+            // Format : "Feature<i>_<j>"
+            oss << "Feature" << feature << "_" << featureCol;
+            // Handle separation
+            if (featureCol != passphraseSize-1) {
+                oss << ",";
+            }
+            csvHeader.push_back(oss.str());
+        }
+    }
+    // Go back to line for the data
+    csvHeader.push_back("\n");
+    return csvHeader;
+}
+
+// Store the data in csv format, add header and save file in /myData directory
+void saveCsvData(UserID userSettings,vector<vector<int>> userData) {
+    // Inits
+    string passphraseSize;
+    // WIP : Handle this cleaner ?
+    if (userSettings.getPassphrase() == "pouet pouet") {
+        passphraseSize = "short";
+    }
+    else if (userSettings.getPassphrase() == "we want a long passphrase") {
+        passphraseSize = "medium";
+    }
+    else if (userSettings.getPassphrase() == "rts stands for network telecommunications and security") {
+        passphraseSize = "long";
+    }
+    // Define the file path and its name
+    const string filename = userSettings.getUsername()+"_"+passphraseSize+".csv";
+    const string filepath = "./myData/"+filename;
+    // Saving file settings
+    ofstream output_file(filepath);
+    // Init the file with the header
+    vector<string> csvHeader = genCsvHeader(userSettings);
+    for (const auto& headerCol : csvHeader) {
+        output_file << headerCol;
+    }
+    // Copy the data in the file
+    for (size_t row = 0; row < userData.size(); row++) {
+        // First add on each row the user's information
+        output_file << userSettings.getUsername() << "," << userSettings.getGender() << "," << userSettings.getDominantHand() << ",";
+        // Then add the content of each row
+        for (size_t col = 0; col < userData[row].size(); col++) {
+            output_file << userData[row][col];
+            // Separate data by a coma if it's not the last element of the row
+            if (col != userData[row].size() - 1) {
+                output_file << ",";
+            }
+        }
+        // After each row, go to newline
+        output_file << endl;
+    }
+    cout << "\nThank you for your participation !!! You can find the data file in " << filepath << endl;
+}
--- InterfaceKbdDB.h
+++ InterfaceKbdDB.h
@@ -11,14 +11,15 @@
 #include <vector>
 #include <algorithm>
 #include <libevdev-1.0/libevdev/libevdev.h>
+#include <fstream>
 
 using namespace std;
 
 // ----- Constants/Global variables -----
-const vector<const string> passphraseChoice = {"short","s","medium","m","long","l"};
-const vector<const string> passphraseList = {"pouet pouet","we want a long passphrase","rts stands for network telecommunications and security"};
-const vector<const string> genderList = {"m","f","male","female","o","other"};
-const vector<const string> handednessList = {"r","l","right","left","b","both"};
+const vector<string> passphraseChoice = {"short","s","medium","m","long","l"};
+const vector<string> passphraseList = {"pouet pouet","we want a long passphrase","rts stands for network telecommunications and security"};
+const vector<string> genderList = {"m","f","male","female","o","other"};
+const vector<string> handednessList = {"r","l","right","left","b","both"};
 constexpr int maxPassphraseAttempt = 10;
 
 // ----- Class declaration -----
@@ -40,7 +41,7 @@ public:
     string getPassphrase();
     // Setters have almost all the same structure so let's define a template method using pointer
     template <typename T>
-    void setParameter(T UserID::*attribute,const T& paramInterface,const vector<const string>& T& paramList);
+    void setParameter(T UserID::*attribute,const T& paramInterface,const vector<string>& paramList);
     // Setters to make user assign the information
     // Note : ask user as input stream, it is why there is no input in the setters
     void setUsername();
@@ -49,9 +50,13 @@ public:
     void setPassphrase();
     // Display information method
     void displayUserID() const;
+    // Gather the information we need
+    void gatherInformation();
 };
 
 // ----- Function declaration -----
 vector<vector<int>> getUserData(UserID userSettings,libevdev* dev);
+vector<string> genCsvHeader(UserID userID);
+void saveCsvData(UserID userSettings,vector<vector<int>> userData);
 
 #endif //INTERFACEKBDDB_H
